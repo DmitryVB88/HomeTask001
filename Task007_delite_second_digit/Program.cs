a56a80ec695d07ad4afdca82a2b1fdd6c663952d
Dima <dima.brashovyan@mail.ru>
2022-05-01 19:23:56 +0300

    Удаляем втрую цифру трёхзначного числа

---
+++ Task007_delite_second_digit/Program.cs
@@ -0,0 +1,16 @@
+﻿//Удалить вторую цифру трехзначного числа. 
+Console.Write("Введите трёхзначное число: ");
+int a = int.Parse(Console.ReadLine());
+if (a > 99 && a < 999)
+{
+    int secdig = (a / 10 % 10);
+    int result = (secdig * 10);
+    int result2 = a - result;
+    Console.Write("Сейчас удалим вторую цифру введённого числа, и вот что получится = ");
+    Console.WriteLine(result2);
+    
+}
+else
+{
+    Console.WriteLine("Введенное число не является трёхзначным!");
+}
