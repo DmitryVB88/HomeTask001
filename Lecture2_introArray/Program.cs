7a7409d426d3045407ae8956872902928d1bfef2
Dima <dima.brashovyan@mail.ru>
2022-05-01 19:15:58 +0300

    Работа с массивами в сишарп

---
+++ Lecture2_introArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {16,12,33,42,73,66,73,28,91};
+
+int n = array.Length;
+int find = 73;
+
+int index = 0;
+
+while (index < n)
+{
+
+      if(array[index] == find)
+      {
+          Console.WriteLine(index);
+          break;
+      } 
+    index++;      
+}
