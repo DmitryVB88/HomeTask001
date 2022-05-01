5ba433154659994e1df34f1018640b13cd84947f
Dima <dima.brashovyan@mail.ru>
2022-05-01 19:18:17 +0300

    Наименование дней недели

---
+++ Task002_Days_of_week/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+int day = int.Parse(Console.ReadLine());
+ 
+if(day < 1 || day > 7)
+{
+    Console.WriteLine("Чё,забыл сколько дней в неделе? Повнимательнее!!!");
+}
+
+if(day == 1)
+{
+    Console.WriteLine("Monday");
+}
+if(day == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+if(day == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+if(day == 4)
+{
+    Console.WriteLine("Thursday");
+}
+if(day == 5)
+{
+    Console.WriteLine("Friday");
+}
+if(day == 6)
+{
+    Console.WriteLine("Saturday");
+}
+if(day == 7)
+{
+    Console.WriteLine("Sunday");
+}
+
