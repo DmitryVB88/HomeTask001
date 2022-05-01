a33068f49bc95b8b4799ee4b6a5f519bfdf01fc9
Dima <dima.brashovyan@mail.ru>
2022-05-01 19:21:46 +0300

    Вторая цифра трёхзначного числа

---
+++ Task005_second_digit_of_3-digit_number/Program.cs
@@ -0,0 +1,14 @@
+﻿//Найти вторую цифру трехзначного числа.
+Console.WriteLine("Введите трёхзначное число:");
+int a = int.Parse(Console.ReadLine());
+if (a > 99 && a < 1000);
+{
+    Console.WriteLine("Вторая цифра трёхзначного числа =");
+    Console.WriteLine(a / 10 % 10);
+    
+}
+
+//else
+
+//      Console.WriteLine("Данное число, не трёхзначное.");
+//}
