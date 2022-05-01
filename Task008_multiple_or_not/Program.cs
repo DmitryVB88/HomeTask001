994df513f72703084ee37a01139755ea75442dcc
Dima <dima.brashovyan@mail.ru>
2022-05-01 19:25:41 +0300

    Картно ли число заданному"

---
+++ Task008_multiple_or_not/Program.cs
@@ -0,0 +1,27 @@
+﻿//Выяснить, кратно ли число заданному, если нет, вывести остаток. 
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+int b = new Random().Next (1,10);
+//Console.WriteLine(a);
+Console.WriteLine(b);
+if(b > a) //в случае, когда введенное число больше сгенерированного, они меняются местами.
+{
+ int n = a;
+    a = b;
+    b = n;
+
+    Console.WriteLine(a);
+    Console.WriteLine(b);
+}
+int mult = a % b;
+    Console.WriteLine(mult);
+if(mult == 0)
+ {
+     Console.WriteLine("Введенное число, кратно сгенерированному!");
+    
+ }
+else
+ {
+     Console.WriteLine("Данной число не кратно сгенерированному, остаток от деления =");
+     Console.WriteLine(mult);
+ }
