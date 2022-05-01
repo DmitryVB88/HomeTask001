689dd87ff1d422b7b5be094d6ac67c45151a8b36
Dima <dima.brashovyan@mail.ru>
2022-05-01 23:59:01 +0300

    Подправили ввод и вывод данных

--- Task005_second_digit_of_3-digit_number/Program.cs
+++ Task005_second_digit_of_3-digit_number/Program.cs
@@ -1,9 +1,9 @@
 ﻿//Найти вторую цифру трехзначного числа.
-Console.WriteLine("Введите трёхзначное число:");
+Console.Write("Введите трёхзначное число: ");
 int a = int.Parse(Console.ReadLine());
 if (a > 99 && a < 1000);
 {
-    Console.WriteLine("Вторая цифра трёхзначного числа =");
+    Console.Write("Вторая цифра трёхзначного числа = ");
     Console.WriteLine(a / 10 % 10);
     
 }
