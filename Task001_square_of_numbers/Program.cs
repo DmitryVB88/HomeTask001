3f3dc2203e9ead843b40291a0cef41f87435201f
Dima <dima.brashovyan@mail.ru>
2022-05-01 19:17:27 +0300

    Равенство одного числа через квадрат другого

---
+++ Task001_square_of_numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите два числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+int x = a * a;
+Console.WriteLine(x);
+
+if(x / b == b)
+  {
+       Console.WriteLine("Квадрат числа A равен квадрату числа B.");
+       Console.WriteLine(b);
+   }
+
+else
+{
+   Console.WriteLine("Тождество не верно...");
+}  
