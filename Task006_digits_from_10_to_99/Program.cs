e0dd8e01d95b43db21d5afc4dc51d7bec17d2beb
Dima <dima.brashovyan@mail.ru>
2022-05-01 19:22:40 +0300

    числа от 10 до 99

---
+++ Task006_digits_from_10_to_99/Program.cs
@@ -0,0 +1,27 @@
+﻿//Дано число от 10 до 99. Указать, какая цифра числа больше.
+int a = new Random().Next(10, 99);
+Console.WriteLine(a);
+int x = (a / 10);
+Console.WriteLine(x);
+int y = (a % 10);
+Console.WriteLine(y);
+if( x > y)
+{
+    Console.WriteLine("max= ");
+    Console.WriteLine(x);
+    Console.WriteLine("min=");
+    Console.WriteLine(y);
+}
+else
+{
+    Console.WriteLine("max=");
+    Console.WriteLine(y);
+    Console.WriteLine("min=");
+    Console.WriteLine(x);
+}
+if (x == y)
+{
+    Console.WriteLine("Числа равны между собой");
+    Console.WriteLine(x);
+    Console.WriteLine(y);
+    }
