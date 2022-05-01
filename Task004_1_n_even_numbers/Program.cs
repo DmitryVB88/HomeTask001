66a0ae11b6c6ce4108298e8b06f99d798a1b3876
Dima <dima.brashovyan@mail.ru>
2022-05-01 19:20:39 +0300

    Четыне числа в последовательности от 1 до N

---
+++ Task004_1_n_even_numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напечатать все чётные числа от 1 до N.
+int min = 1;
+int max = new Random().Next(2,100);
+Console.WriteLine(min);
+Console.WriteLine(max);
+
+while(min <= max)
+{
+     if(min % 2 == 0)
+     Console.Write(" " + min);
+     min++;
+
+}
