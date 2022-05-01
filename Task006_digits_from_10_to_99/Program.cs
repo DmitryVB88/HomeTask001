9fbd81b2487b6b64c065ab26ea40e0b5e81e192f
Dima <dima.brashovyan@mail.ru>
2022-05-02 00:07:07 +0300

    Добавил пояснение ввода числа

--- Task006_digits_from_10_to_99/Program.cs
+++ Task006_digits_from_10_to_99/Program.cs
@@ -1,27 +1,28 @@
 ﻿//Дано число от 10 до 99. Указать, какая цифра числа больше.
+Console.Write("Программа сгенерировала следующее число: ");
 int a = new Random().Next(10, 99);
 Console.WriteLine(a);
 int x = (a / 10);
-Console.WriteLine(x);
+//Console.WriteLine(x);
 int y = (a % 10);
-Console.WriteLine(y);
+//Console.WriteLine(y);
 if( x > y)
 {
-    Console.WriteLine("max= ");
+    Console.Write("max = ");
     Console.WriteLine(x);
-    Console.WriteLine("min=");
+    Console.Write("min = ");
     Console.WriteLine(y);
 }
 else
 {
-    Console.WriteLine("max=");
+    Console.Write("max = ");
     Console.WriteLine(y);
-    Console.WriteLine("min=");
+    Console.Write("min = ");
     Console.WriteLine(x);
 }
 if (x == y)
 {
-    Console.WriteLine("Числа равны между собой");
+    Console.Write("Числа равны между собой = ");
     Console.WriteLine(x);
     Console.WriteLine(y);
     }
