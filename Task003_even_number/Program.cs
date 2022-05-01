11962df29f9c8e01b52fc57c92c043c32157d914
Dima <dima.brashovyan@mail.ru>
2022-05-01 19:19:05 +0300

    Четные числа

---
+++ Task003_even_number/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+int div = (a % 2);
+
+if(div == 0)
+{
+    Console.WriteLine("Это число чётное!");
+}
+
+else
+{
+    Console.WriteLine("Это число нечётное! В следующий раз, хорошенько подумай, прежде чем что-то на клавиатуре набирать.");
+}
