916c2637a7e36b25f7664054c59b2c34e8c9b0ea
Dima <dima.brashovyan@mail.ru>
2022-05-03 22:02:53 +0300

    Решение без багов. Проанализаровал решение Стоуна.

--- Task006_digits_from_10_to_99/Program.cs
+++ Task006_digits_from_10_to_99/Program.cs
@@ -1,41 +1,59 @@
-﻿//Дано число от 10 до 99. Указать, какая цифра числа больше.
-Console.Write("Программа сгенерировала следующее число: ");
-int a = new Random().Next(10, 99);
-Console.WriteLine(a);
-int x = (a / 10);
+﻿ //Дано число от 10 до 99. Указать, какая цифра числа больше.
+Console.Write("Программа сгенерировала число: ");
+int number = new Random().Next(10, 99);
+Console.WriteLine(number); 
+int x = (number / 10);
 //Console.WriteLine(x);
-int y = (a % 10);
+int y = (number % 10);
 //Console.WriteLine(y);
-
-if (x == y)
-{
-    Console.Write("Первая цифра: ");
-    Console.WriteLine(x);
-    Console.Write("Вторая цифра: ");
-    Console.WriteLine(y);
-    Console.Write("Цифры равны между собой! ");
-    
-    }
-
 if( x > y)
-{
-    Console.Write("max = ");
-    Console.WriteLine(x);
-    Console.Write("min = ");
-    Console.WriteLine(y);
-}
-else
-{
-    Console.Write("max = ");
-    Console.WriteLine(y);
-    Console.Write("min = ");
-    Console.WriteLine(x);
-}
-// if (x == y)
-// {
-//     Console.Write("Первая цифра: ");
-//     Console.WriteLine(x);
-//     Console.Write("Вторая цифра: ");
-//     Console.WriteLine(y);
-//     Console.Write("Цифры равны между собой! ");
+ {
+     Console.Write("max = ");
+     Console.WriteLine(x);
+     Console.Write("min = ");
+     Console.WriteLine(y);
+ }
+ if( y > x)
+ {
+     Console.Write("max = ");
+     Console.WriteLine(y);
+     Console.Write("min = ");
+     Console.WriteLine(x);
+ }
+ if (x == y)
+ {
+     Console.Write("Первая цифра: ");
+     Console.WriteLine(x);
+     Console.Write("Вторая цифра: ");
+     Console.WriteLine(y);
+     Console.Write($"В числе {number} обе цифры равны между собой! ");
+    
+ }
+
+
+//СТОУН!!!
+// Console.WriteLine("Задание. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.");
+// Console.Write("Введите двухзначное число: ");
+// int Number2 = int.Parse(Console.ReadLine() ?? "0");
+// if (10 <= Number2 && Number2 <= 99)
+//     {
+//         int Digit1 = Number2%10;
+//         int Digit2 = Number2/10;
+//         if (Digit1 > Digit2)
+//             {
+//                 Console.WriteLine($"В числе {Number2} наибольшая цифра {Digit1}.");
+//             }
+//         if (Digit2 > Digit1)
+//             {
+//                 Console.WriteLine($"В числе {Number2} наибольшая цифра {Digit2}.");
+//             }
+//         if (Digit2 == Digit1)
+//             {
+//                 Console.WriteLine($"Поздравляем! У вас близнецы! Обе цифры {Digit1}.");
+//             }
+        
+//     }
+// else
+//     {
+//         Console.WriteLine($"А какая разница какая из цифр больше, если у Вас с двузначными числами проблема?");
 //     }
